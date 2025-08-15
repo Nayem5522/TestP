a09faef414ba0cd3d139aa8de6fb828b637c22c3
Nayem5522 <sahidhasannayembd@gmail.com>
2025-08-15 06:08:19 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -875,5 +875,4 @@ def telegram_webhook():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)```
---- দ্বিতীয় অংশ শেষ ---
+    app.run(host='0.0.0.0', port=port, debug=False)
