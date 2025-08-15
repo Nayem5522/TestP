524b86f9a2f6c73b69a50ea92972279b0e2f8bdc
Nayem5522 <sahidhasannayembd@gmail.com>
2025-08-15 16:34:31 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -186,7 +186,7 @@ index_html = """
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
-<title>MovieZone - Your Entertainment Hub</title>
+<title>PmwBD - Your Entertainment Hub with Prime</title>
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:wght@400;500;700&display=swap');
   :root { --netflix-red: #E50914; --netflix-black: #141414; --text-light: #f5f5f5; --text-dark: #a0a0a0; --nav-height: 60px; }
