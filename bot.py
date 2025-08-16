d5e00fddb69196d0b971f577f31d1689738a4509
Nayem5522 <sahidhasannayembd@gmail.com>
2025-08-16 08:47:03 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1824,4 +1824,4 @@ def send_manual_notification(movie_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)```
+    app.run(host='0.0.0.0', port=port, debug=False)
